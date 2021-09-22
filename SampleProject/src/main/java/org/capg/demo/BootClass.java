58563d0205d13def3f3f64849500596a2de63486
adilsidd <mohdad@LIN51015768.corp.capgemini.com>
2021-09-22 20:30:16 +0530

    My First Commit

---
+++ SampleProject/src/main/java/org/capg/demo/BootClass.java
@@ -0,0 +1,11 @@
+package org.capg.demo;
+
+public class BootClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi this a class in STS Workspace!!");
+
+	}
+
+}
